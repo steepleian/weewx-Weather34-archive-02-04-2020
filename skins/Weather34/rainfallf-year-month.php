6d186f7043cbd80248a99a55b08d41644eb3abc9
Ian Millard <steepleian@gmail.com>
2020-03-15 07:49:18 +0000

    Changed from rain rate to rain totals

--- skins/Weather34/rainfallf-year-month.php
+++ skins/Weather34/rainfallf-year-month.php
@@ -3,19 +3,20 @@
 
 <?php //rain month 
 if($weather["rainmmax"]>=1000){ echo "<topblue1>".round($weather["rainmmax"],0)  ;}
-else  echo "<topblue1>".$weather["rainmmax"]  ;echo "<smallwindunit>".$weather["rain_units"];
+else  echo "<topblue1>".$weather["rain_month"]  ;echo "<smallwindunit>".$weather["rain_units"];
 ?>
 </div></smallwindunit>
 <div class="minword"><?php echo date('M')?></div></div>
-<div class="mintimedate">Max 
+<div class="mintimedate">Total 
 </div>  
 
 <div class="yearwordbig"><?php echo date('Y')?></div>
 <div class="topmax">
 <?php //rain year 
 if($weather["rainymax"]>=1000){ echo "<topblue1>".round($weather["rainymax"],0)  ;}
-else  echo "<topblue1>".$weather["rainymax"]  ;echo "<smallwindunit>".$weather["rain_units"];
+else  echo "<topblue1>".$weather["rain_year"]  ;echo "<smallwindunit>".$weather["rain_units"];
 ?>
 </div></smallwindunit>
 <div class="maxword"><?php echo date('Y')?></div></div>
-<div class="maxtimedate">Max</div> 
+<div class="maxtimedate">Total</div> 
+
